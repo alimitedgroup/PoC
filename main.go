9fda62bdabfff78033e7ec17afc2bb21a357751e
Loris Libralato <lorislibra21@gmail.com>
2024-12-04 09:46:26 +0100

    refactor sql queries

--- main.go
+++ main.go
@@ -14,7 +14,22 @@ import (
 
 const dbConnStr = "host=postgres user=postgres password=postgres dbname=database sslmode=disable"
 
-func listenEvents(nc *nats.Conn) *nats.Subscription {
+type AddStockEvent struct {
+	MerceId int64 `json:"merce_id"`
+	Stock   int64 `json:"stock"`
+}
+
+type OrderMerce struct {
+	MerceId int64 `json:"merce_id"`
+	Stock   int64 `json:"stock"`
+}
+type CreateOrderEvent struct {
+	OrderId int64        `json:"order_id"`
+	Note    string       `json:"note"`
+	Merci   []OrderMerce `json:"merci"`
+}
+
+func ListenEvents(nc *nats.Conn) *nats.Subscription {
 	// Subscribe to a subject
 	sub, err := nc.Subscribe("warehouse_events.*", func(m *nats.Msg) {
 		log.Printf("Received a message: %s\n", string(m.Data))
@@ -25,42 +40,93 @@ func listenEvents(nc *nats.Conn) *nats.Subscription {
 	return sub
 }
 
-func insertStockMerce(db *sql.DB) {
-	type AddStockEvent struct {
-		MerceId int64 `json:"merce_id"`
-		Stock   int64 `json:"stock"`
-	}
-	addStockEvent := AddStockEvent{
-		MerceId: 1,
-		Stock:   10,
-	}
-	var stmt *sql.Stmt
-	var res sql.Result
-	var rowCount int64
-	var err error
+type Repo struct{}
 
-	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	stmt, err = tx.Prepare("UPDATE merce SET stock=stock+$2 WHERE id = $1")
+func (r *Repo) CreateOrder(tx *sql.Tx, note string) int64 {
+	var orderId int64
+	stmt, err := tx.Prepare("INSERT INTO orders (note) VALUES ($1) RETURNING id")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
 
-	res, err = stmt.Exec(addStockEvent.MerceId, addStockEvent.Stock)
+	err = stmt.QueryRow(note).Scan(&orderId)
 	if err != nil {
 		log.Fatal(err)
 	}
-	rowCount, err = res.RowsAffected()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Updated %d row(s)\n", rowCount)
 
-	stmt, err = tx.Prepare("INSERT INTO merce_event (message) VALUES ($1)")
+	log.Printf("Inserted order with id: %d\n", orderId)
+	return orderId
+}
+
+func (r *Repo) InsertOrderMerce(tx *sql.Tx, orderId int64, merceId int64, stock int64) {
+	stmt, err := tx.Prepare("INSERT INTO order_merce (order_id, merce_id, stock) VALUES ($1, $2, $3)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(orderId, merceId, stock)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rowCount, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if rowCount != 1 {
+		log.Fatalf("Expected to insert 1 row, inserted %d rows\n", rowCount)
+	}
+}
+
+func (r *Repo) IncreaseStockMerce(tx *sql.Tx, merceId int64, stock int64) {
+	stmt, err := tx.Prepare("UPDATE merce SET stock=stock+$2 WHERE id = $1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(merceId, stock)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rowCount, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if rowCount != 1 {
+		log.Fatalf("Expected to update 1 row, updated %d rows\n", rowCount)
+	}
+}
+
+func (r *Repo) DecrementStockMerce(tx *sql.Tx, merceId int64, stock int64) {
+	stmt, err := tx.Prepare("UPDATE merce SET stock=stock-$2 WHERE id = $1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(merceId, stock)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rowCount, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if rowCount != 1 {
+		log.Fatalf("Expected to update 1 row, updated %d rows\n", rowCount)
+	}
+}
+
+func (r *Repo) InsertAddMerceStockEvent(tx *sql.Tx, addStockEvent AddStockEvent) {
+	stmt, err := tx.Prepare("INSERT INTO merce_event (message) VALUES ($1)")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -71,111 +137,23 @@ func insertStockMerce(db *sql.DB) {
 		log.Fatal(err)
 	}
 
-	res, err = stmt.Exec(data)
+	res, err := stmt.Exec(data)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rowCount, err = res.RowsAffected()
+	rowCount, err := res.RowsAffected()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Updated %d row(s)\n", rowCount)
 
-	defer func() {
-		if err != nil {
-			log.Println("Rolling back transaction due to error...")
-			if rollbackErr := tx.Rollback(); rollbackErr != nil {
-				log.Fatalf("Failed to rollback transaction: %v", rollbackErr)
-			}
-			return
-		}
-		// Commit the transaction if no error
-		if commitErr := tx.Commit(); commitErr != nil {
-			log.Fatalf("Failed to commit transaction: %v", commitErr)
-		}
-		log.Println("Transaction committed successfully.")
-	}()
-
+	if rowCount != 1 {
+		log.Fatalf("Expected to insert 1 row, inserted %d rows\n", rowCount)
+	}
 }
 
-func insertOrder(db *sql.DB) {
-	type OrderMerce struct {
-		MerceId int64 `json:"merce_id"`
-		Stock   int   `json:"stock"`
-	}
-	type OrderEvent struct {
-		OrderId int64        `json:"order_id"`
-		Note    string       `json:"note"`
-		Merci   []OrderMerce `json:"merci"`
-	}
-
-	orderEvent := OrderEvent{
-		Note: "test order",
-		Merci: []OrderMerce{
-			{
-				MerceId: 1,
-				Stock:   3,
-			},
-		},
-	}
-
-	var stmt *sql.Stmt
-	var res sql.Result
-	var rowCount int64
-	var err error
-
-	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	stmt, err = tx.Prepare("INSERT INTO orders (note) VALUES ($1) RETURNING id")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	err = stmt.QueryRow(orderEvent.Note).Scan(&orderEvent.OrderId)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("Inserted order with id: %d\n", orderEvent.OrderId)
-
-	stmt, err = tx.Prepare("INSERT INTO order_merce (order_id, merce_id, stock) VALUES ($1, $2, $3)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-	res, err = stmt.Exec(orderEvent.OrderId, orderEvent.Merci[0].MerceId, orderEvent.Merci[0].Stock)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rowCount, err = res.RowsAffected()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Inserted %d row(s)\n", rowCount)
-
-	stmt, err = tx.Prepare("UPDATE merce SET stock=stock-$2 WHERE id = $1")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	res, err = stmt.Exec(orderEvent.Merci[0].MerceId, orderEvent.Merci[0].Stock)
-	if err != nil {
-		log.Fatal(err)
-	}
-	rowCount, err = res.RowsAffected()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Updated %d row(s)\n", rowCount)
-
-	stmt, err = tx.Prepare("INSERT INTO order_event (message) VALUES ($1)")
+func (r *Repo) InsertCreateOrderEvent(tx *sql.Tx, orderEvent CreateOrderEvent) {
+	stmt, err := tx.Prepare("INSERT INTO order_event (message) VALUES ($1)")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -186,31 +164,73 @@ func insertOrder(db *sql.DB) {
 		log.Fatal(err)
 	}
 
-	res, err = stmt.Exec(data)
+	res, err := stmt.Exec(data)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rowCount, err = res.RowsAffected()
+	rowCount, err := res.RowsAffected()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Inserted %d row(s)\n", rowCount)
 
-	defer func() {
-		if err != nil {
-			log.Println("Rolling back transaction due to error...")
-			if rollbackErr := tx.Rollback(); rollbackErr != nil {
-				log.Fatalf("Failed to rollback transaction: %v", rollbackErr)
-			}
-			return
-		}
-		// Commit the transaction if no error
-		if commitErr := tx.Commit(); commitErr != nil {
-			log.Fatalf("Failed to commit transaction: %v", commitErr)
-		}
-		log.Println("Transaction committed successfully.")
-	}()
+	if rowCount != 1 {
+		log.Fatalf("Expected to insert 1 row, inserted %d rows\n", rowCount)
+	}
+}
+
+func (r *Repo) Commit(tx *sql.Tx) {
+	if err := tx.Commit(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func InsertStockMerce(db *sql.DB) {
+	addStockEvent := AddStockEvent{
+		MerceId: 1,
+		Stock:   10,
+	}
+	var err error
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := Repo{}
+	defer repo.Commit(tx)
+
+	repo.IncreaseStockMerce(tx, addStockEvent.MerceId, addStockEvent.Stock)
+	repo.InsertAddMerceStockEvent(tx, addStockEvent)
+}
+
+func InsertOrder(db *sql.DB) {
+	orderEvent := CreateOrderEvent{
+		Note: "test order",
+		Merci: []OrderMerce{
+			{
+				MerceId: 1,
+				Stock:   3,
+			},
+		},
+	}
+
+	var err error
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := Repo{}
+	defer repo.Commit(tx)
+
+	orderEvent.OrderId = repo.CreateOrder(tx, orderEvent.Note)
+	for _, merce := range orderEvent.Merci {
+		repo.InsertOrderMerce(tx, orderEvent.OrderId, merce.MerceId, merce.Stock)
+		repo.DecrementStockMerce(tx, merce.MerceId, merce.Stock)
+	}
+	repo.InsertCreateOrderEvent(tx, orderEvent)
 }
 
 func main() {
@@ -221,7 +241,7 @@ func main() {
 	}
 	defer nc.Close()
 	log.Println("Connected to NATS server")
-	sub := listenEvents(nc)
+	sub := ListenEvents(nc)
 	defer sub.Unsubscribe()
 
 	// Connect to the database
@@ -238,8 +258,8 @@ func main() {
 	fmt.Println("Connected to PostgreSQL!")
 
 	for {
-		insertStockMerce(db)
-		insertOrder(db)
+		InsertStockMerce(db)
+		InsertOrder(db)
 		time.Sleep(1 * time.Second)
 	}
 }
