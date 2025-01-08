b6a8501337df6575d96456c7499a7ff9b189be35
esposm03 <36164633+esposm03@users.noreply.github.com>
2025-01-08 17:53:25 +0100

    Implement the beginning of a service abstraction

---
+++ common/natsutil/service.go
@@ -0,0 +1,157 @@
+package natsutil
+
+import (
+	"context"
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+	"log/slog"
+)
+
+// Service collects and unifies various functionality that would otherwise be repeated among all services
+//
+// In particular, it implements cancellation with a context, automatic tracing of requests and responses
+type Service[S any] struct {
+	state         S
+	ctx           context.Context
+	nc            *nats.Conn
+	js            jetstream.JetStream
+	subscriptions []*nats.Subscription
+}
+
+// Handler represents a handler for a particular NATS Core subject
+type Handler[S any] func(context.Context, *Service[S], *nats.Msg)
+
+// JsHandler represents a handler for a particular JetStream stream
+type JsHandler[S any] func(context.Context, *Service[S], jetstream.Msg) error
+
+// NewService will create a new instance of Service
+func NewService[S any](ctx context.Context, nc *nats.Conn, state S) *Service[S] {
+	js, err := jetstream.New(nc)
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to create JetStream client", "error", err)
+		return nil
+	}
+
+	s := &Service[S]{ctx: ctx, state: state, nc: nc, js: js}
+
+	go func(ctx context.Context) {
+		<-ctx.Done()
+
+		for _, sub := range s.subscriptions {
+			err := sub.Drain()
+			if err != nil {
+				slog.ErrorContext(ctx, "Failed to drain subscription", "error", err, "subject", sub.Subject)
+			}
+		}
+	}(ctx)
+
+	return s
+}
+
+// State returns the state of this service.
+//
+// Note that the state can be shared between multiple goroutines,
+// so you should implement locking if it is needed.
+func (s *Service[S]) State() *S {
+	return &s.state
+}
+
+// NatsConn returns the NATS connection associated with this Service
+func (s *Service[S]) NatsConn() *nats.Conn {
+	return s.nc
+}
+
+// JetStream returns the JetStream connection associated with this Service
+func (s *Service[S]) JetStream() jetstream.JetStream {
+	return s.js
+}
+
+// RegisterHandler registers a handler for the given NATS subject
+func (s *Service[S]) RegisterHandler(subject string, handler Handler[S]) error {
+	subscription, err := s.NatsConn().Subscribe(subject, func(msg *nats.Msg) {
+		handler(s.ctx, s, msg)
+	})
+	if err != nil {
+		return err
+	}
+
+	s.subscriptions = append(s.subscriptions, subscription)
+
+	return nil
+}
+
+// RegisterJsHandlerExisting registers a handler for the given JetStream stream.
+//
+// An ad-hoc, ephemeral consumer will be created for the given stream using the given opts.
+// After having read all messages (i.e. when a messages is returned with NumPending = 0),
+// then the subscription will be automatically closed, and only then this function will return.
+func (s *Service[S]) RegisterJsHandlerExisting(stream string, handler JsHandler[S], opts ...JsHandlerOpt) error {
+	cfg := jetstream.ConsumerConfig{}
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+
+	consumer, err := s.js.CreateConsumer(s.ctx, stream, cfg)
+	if err != nil {
+		return fmt.Errorf("failed to create JetStream consumer: %w", err)
+	}
+
+	// Consume all messages, and stop when they are finished or an error occurs
+	var cc jetstream.ConsumeContext
+	var msgErr error
+	cc, err = consumer.Consume(func(msg jetstream.Msg) {
+		msgErr = handler(s.ctx, s, msg)
+		if msgErr != nil {
+			err = fmt.Errorf("failed to handle message: %w", msgErr)
+			cc.Stop()
+		}
+
+		var meta *jetstream.MsgMetadata
+		meta, msgErr = msg.Metadata()
+		if msgErr != nil {
+			err = fmt.Errorf("failed to read message metadata: %w", msgErr)
+			cc.Stop()
+		}
+		if msgErr == nil && meta.NumPending == 0 {
+			cc.Drain()
+		}
+	})
+	if err != nil {
+		return fmt.Errorf("failed to consume from stream: %w", err)
+	}
+
+	<-cc.Closed()
+	return msgErr
+}
+
+// JsHandlerOpt represents various options used when creating a JetStream handler
+type JsHandlerOpt func(config *jetstream.ConsumerConfig)
+
+// WithDeliverNew will set the consumer's DeliveryPolicy to DeliverNew
+func WithDeliverNew() JsHandlerOpt {
+	return func(config *jetstream.ConsumerConfig) {
+		config.DeliverPolicy = jetstream.DeliverNewPolicy
+	}
+}
+
+// WithDeliverAll will set the consumer's DeliveryPolicy to DeliverAll
+func WithDeliverAll() JsHandlerOpt {
+	return func(config *jetstream.ConsumerConfig) {
+		config.DeliverPolicy = jetstream.DeliverAllPolicy
+	}
+}
+
+// WithSubjectFilter will filter the delivered messages to those specified. Mutually exclusive with WithSubjectsFilter
+func WithSubjectFilter(subject string) JsHandlerOpt {
+	return func(config *jetstream.ConsumerConfig) {
+		config.FilterSubject = subject
+	}
+}
+
+// WithSubjectsFilter will filter the delivered messages to those specified. Mutually exclusive with WithSubjectFilter
+func WithSubjectsFilter(subjects []string) JsHandlerOpt {
+	return func(config *jetstream.ConsumerConfig) {
+		config.FilterSubjects = append(config.FilterSubjects, subjects...)
+	}
+}
--- common/natsutils.go
+++ common/natsutils.go
@@ -2,6 +2,11 @@ package common
 
 import (
 	"context"
+	natsserver "github.com/nats-io/nats-server/v2/server"
+	"github.com/nats-io/nats.go"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -9,6 +14,33 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 )
+
+// NewInProcessNATSServer creates a temporary, in-process NATS server, and returns a connection to it. Useful for tests
+func NewInProcessNATSServer(t *testing.T) *nats.Conn {
+	tmp, err := os.MkdirTemp("", "nats_test")
+	require.NoError(t, err)
+
+	server, err := natsserver.NewServer(&natsserver.Options{
+		DontListen: true,
+		JetStream:  true,
+		StoreDir:   tmp,
+	})
+	require.NoError(t, err)
+
+	server.Start()
+	t.Cleanup(func() {
+		server.Shutdown()
+		err := os.RemoveAll(tmp)
+		require.NoError(t, err)
+	})
+	require.True(t, server.ReadyForConnections(1*time.Second))
+
+	// Create a connection.
+	conn, err := nats.Connect("", nats.InProcessServer(server))
+	require.NoError(t, err)
+
+	return conn
+}
 
 // StatefulHandler is a utility type for creating NATS handlers that get passed
 // a context.Context, and some shared state that is necessary for handling the request.
