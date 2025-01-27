0d92f9476b5d9bf23308c0b67497022252478794
esposm03 <36164633+esposm03@users.noreply.github.com>
2025-01-27 22:27:33 +0100

    cli: fetch warehouses

---
+++ cli/fetchers.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"log"
+	"net/http"
+)
+
+var client http.Client
+
+type NewWarehousesMsg struct {
+	warehouses []string
+}
+
+func FetchWarehouses() tea.Msg {
+	resp, err := client.Get(fmt.Sprintf("%s/warehouses", *apiGateway))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var warehouses []string
+	err = json.NewDecoder(resp.Body).Decode(&warehouses)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return NewWarehousesMsg{warehouses}
+}
